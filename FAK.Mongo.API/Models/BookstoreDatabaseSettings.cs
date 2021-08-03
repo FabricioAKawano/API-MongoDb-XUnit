269eabd774e573db3af5052e984c629ac76fd2dc
Fabrício Aparecido Kawano <fabriciokawano@hotmail.com>
2021-08-02 23:51:35 -0300

    Add project files.

---
+++ FAK.Mongo.API/Models/BookstoreDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAK.Mongo.API.Models
+{
+    public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string BooksCollectionName { get; set; }
+    }
+
+    public interface IBookstoreDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+        string BooksCollectionName { get; set; }
+    }
+}
---
+++ FAK.Mongo.API/Services/BookService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FAK.Mongo.API.Models;
+using MongoDB.Driver;
+
+namespace FAK.Mongo.API.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var db = client.GetDatabase(settings.DatabaseName);
+            _books = db.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() => _books.Find(b => true).ToList();
+    }
+}
